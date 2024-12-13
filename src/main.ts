9b9d7a2e667b370ccd82d4030f89f30785d1bcc2
Linus Rachlis <linus@rachlis.net>
2024-12-13 17:40:22 -0500

    Add brush preview for colour/size options

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.width = window.visualViewport!.width;
     canvas.height = window.visualViewport!.height;
     const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
-    ctx.fillStyle = 'red';
-    let size = 10;
+    let colour: string;
+    let size: number;
     let activePointerIds: number[] = [];
-
-    const changeFillStyle = (fillStyle: string) => {
-        ctx.fillStyle = fillStyle;
-    };
-    const changeSize = (newSize: number) => {
-        size = newSize;
-    };
-    const draw = (size: number, x: number, y: number): void => {
-        ctx.fillRect(x - size / 2, y - size / 2, size, size);
-    };
 
     const button_color_black = document.querySelector<HTMLButtonElement>(
         '#button_color_black',
@@ -35,32 +25,66 @@ document.addEventListener('DOMContentLoaded', () => {
     const button_color_white = document.querySelector<HTMLButtonElement>(
         '#button_color_white',
     )!;
+    const previewElement = document.querySelector<HTMLDivElement>('#preview')!;
     const input_size = document.querySelector<HTMLInputElement>('#input_size')!;
 
+    const changeColour = (newColour: string): void => {
+        colour = newColour;
+        ctx.fillStyle = newColour;
+    };
+    const changeSize = (newSize: number): void => {
+        size = newSize;
+    };
+    const updatePreview = (): void => {
+        previewElement.style.width = `${size}px`;
+        previewElement.style.height = `${size}px`;
+        previewElement.style.backgroundColor = colour;
+    };
+    const draw = (size: number, x: number, y: number): void => {
+        ctx.fillRect(x - size / 2, y - size / 2, size, size);
+    };
+
+    // Initialize options
+    changeColour('red');
+    changeSize(10);
+    updatePreview();
+
     button_color_black.addEventListener('click', () => {
-        changeFillStyle('black');
+        changeColour('black');
+        updatePreview();
     });
     button_color_red.addEventListener('click', () => {
-        changeFillStyle('red');
+        changeColour('red');
+        updatePreview();
     });
     button_color_green.addEventListener('click', () => {
-        changeFillStyle('green');
+        changeColour('green');
+        updatePreview();
     });
     button_color_yellow.addEventListener('click', () => {
-        changeFillStyle('yellow');
+        changeColour('yellow');
+        updatePreview();
     });
     button_color_blue.addEventListener('click', () => {
-        changeFillStyle('blue');
+        changeColour('blue');
+        updatePreview();
     });
     button_color_white.addEventListener('click', () => {
-        changeFillStyle('white');
+        changeColour('white');
+        updatePreview();
     });
 
     input_size.addEventListener('change', (e) => {
         changeSize(parseInt((e.target as HTMLInputElement).value));
+        updatePreview();
     });
 
-    // This works on everything I tried except for Firefox Android. For some reason a second touch pointer stops after the first 'pointermove' and no more events seem to come for that pointer. Not sure if that's a mistake I've made or something wrong with FF Android specifically. Maybe need to experiment with https://patrickhlauke.github.io/touch/ on FF Android to see if not all expected events are received.
+    // This works on everything I tried except for Firefox Android. For some
+    // reason a second touch pointer stops after the first 'pointermove' and no
+    // more events seem to come for that pointer. Not sure if that's a mistake
+    // I've made or something wrong with FF Android specifically. Maybe need to
+    // experiment with https://patrickhlauke.github.io/touch/ on FF Android to
+    // see if not all expected events are received.
     canvas.addEventListener('pointerdown', (e) => {
         activePointerIds.push(e.pointerId);
         draw(size, e.offsetX, e.offsetY);
